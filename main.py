34bc79acad5c43d925190c5f0f4c91d9958b77ad
HSUHAOBM <hao66bmbm@gmail.com>
2022-01-22 15:43:02 +0800

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='myredis', port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    redis.expire('hits', 600) 
+    return '你好! 我們見過 %s 次面。' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5800)
