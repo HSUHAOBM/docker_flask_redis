351ec3ccc4d8d3beb850928e1b456572f1e482fa
HSUHAOBM <hao66bmbm@gmail.com>
2022-02-15 14:12:20 +0800

    test for jenkins5

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ redis = Redis(host='myredis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    redis.expire('hits', 6000) 
+    redis.expire('hits', 6600) 
     return '你好! 我們見過 %s 次面。' % str(redis.get('hits'))
 
 if __name__ == "__main__":
