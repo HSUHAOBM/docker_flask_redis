3357c6ef0ecca1a8ff04a552fc875c13f80c12db
Ian_VM <hao66bmbm@gmail.com>
2023-10-24 13:19:12 +0800

    test code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
+from datetime import datetime
 
 app = Flask(__name__)
 redis = Redis(host='myredis', port=6379)
@@ -8,8 +9,13 @@ redis = Redis(host='myredis', port=6379)
 def hello():
     redis.incr('hits')
     redis.expire('hits', 6600)
-    num = redis.get('hits').decode("utf-8") 
+    num = redis.get('hits').decode("utf-8")
     return 'server 1 - 你好! 我們見過 %s 次面。' % num
+
+@app.route('/test')
+def index():
+    current_time = datetime.now()
+    return render_template('index.html', current_time=current_time)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5800)
